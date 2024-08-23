7f9557e9d733ec8b8ab608cd487a8e713b7d3c07
hyuna7131 <hyunal7131@gmail.com>
2024-08-23 13:43:54 +0900

    setting environment variable

--- notice board/app/database.py
+++ notice board/app/database.py
@@ -9,17 +9,15 @@ load_dotenv()
 def get_connection():
     try:
         connection = pymysql.connect(
-            host=os.getenv('DB_HOST'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            db=os.getenv('DB_NAME'),
-            charset=os.getenv('DB_CHARSET'),
+            host=os.getenv('DATABASE_HOST'),
+            user=os.getenv('DATABASE_USER'),
+            password=os.getenv('DATABASE_PASSWORD'),
+            db=os.getenv('DATABASE_NAME'),
+            charset=os.getenv('DATABASE_CHARSET'),
             cursorclass=pymysql.cursors.DictCursor,
-            port=int(os.getenv('DB_PORT'))  # 포트는 int로 변환해야 함
+            port=int(os.getenv('DATABASE_PORT', 3306))  # 포트는 int로 변환해야 함
         )
         return connection
     except OperationalError as e:
         print(f"Error: Unable to connect to the database. {e}")
         raise
-
-# 이 함수는 데이터베이스에 연결할 때 호출됩니다.
